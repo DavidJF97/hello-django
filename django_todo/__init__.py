406247f98505fb8bb8817baee284943b616236e4
David J-F <davidjf97@yahoo.ie>
2022-12-04 20:01:44 +0000

    Fix issue with hiding secret key

--- django_todo/__init__.py
+++ django_todo/__init__.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask
-if os.path.exists("env.py"):
-    import env
+# if os.path.exists("env.py"):
+#     import env
+from env import *
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+# app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
